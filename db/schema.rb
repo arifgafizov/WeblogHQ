80eec0511160ac48459746097d5d2875f0e671ef
Arif <agafizov@gmail.com>
2019-11-09 18:39:29 +0300

    Create table comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_07_130410) do
+ActiveRecord::Schema.define(version: 2019_11_09_152655) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text "content_com"
+    t.integer "post_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
